47473f26383cb201f97f002e631bfbc01f54d688
Diego Magdaleno <dmmagdal@ucsc.edu>
2024-06-23 13:09:16 -0400

    Updated convert_w2d.py to be able to run on existing data.

--- convert_w2d.py
+++ convert_w2d.py
@@ -8,8 +8,10 @@
 # Windows/MacOS/Linux
 
 
+import argparse
 import hashlib
 import json
+import os
 
 import msgpack
 from tqdm import tqdm
@@ -35,7 +37,13 @@ def hashSum(data: str) -> str:
 	return sha256.hexdigest()
 
 
-def main():
+def test():
+	"""
+	Test out the necessary steps to first convert the JSON data from 
+		str: List[str] to str: int for the word to doc metadata.
+	@param: takes no arguments.
+	@return: returns nothing.
+	"""
 	# Target and source files.
 	source_file = "pages-articles-multistream10_288ec473bfbc27d7686cfee3396c7ea4db31c2f7b2b967778e1e3a21c08a583b_w2d.json"
 	target1_file = "pages-articles-multistream10_288ec473bfbc27d7686cfee3396c7ea4db31c2f7b2b967778e1e3a21c08a583b_w2d_count.json"
@@ -97,6 +105,117 @@ def main():
 	print(f"Object match: {contents == loaded_contents}")
 	print(f"Hash match: {original_hash == loaded_hash}")
 	print(f"Size match: {original_size == load_size}")
+
+
+def convert():
+	"""
+	Convert the existing metadata (JSON data) str: List[str] to 
+		str: int for the word to doc metadata.
+	@param: takes no arguments.
+	@return: returns nothing.
+	"""
+	# Initialize/load tracking information for files already converted.
+	tracking_file = "tracking.txt"
+	current_progress = []
+	if not os.path.exists(tracking_file):
+		open(tracking_file, "w+").close()
+	else:
+		with open(tracking_file, "r") as f:
+			current_progress = f.readlines()
+
+		current_progress = [
+			file.rstrip("\n") for file in current_progress
+		]
+
+	# Load config JSON.
+	# with open("config.json", "r") as config_f:
+	# 	config = json.load(config_f)
+
+	doc_to_words_path = "./metadata/bag_of_words"
+
+	if not os.path.exists(doc_to_words_path):
+		print("Path to doc_to_words JSON file does not exist")
+		return
+
+	json_files = [
+		file for file in os.listdir(doc_to_words_path) 
+		if file.endswith(".json")
+	]
+
+	# Iterate through each file.
+	for file in tqdm(json_files):
+		# Skip files already processed.
+		if file in current_progress:
+			continue
+
+		# Load the original data.
+		with open(file, "r") as w2d_fr:
+			data = json.load(w2d_fr)
+
+		# Iterate through the data overriding the values (List[str])
+		# with their new value.
+		for key, value in data.items():
+			assert isinstance(value, list)
+			data[key] = len(value)
+
+		# Write the data back to the JSON file.
+		with open(file, "w") as w2d_fw:
+			json.dump(w2d_fw, data, indent=4)
+
+		# Convert the JSON data to string and hash it.
+		data_str = json.dumps(data, sort_keys=True)
+		data_hash = hashSum(data_str)
+
+		# Save the data to msgpack.
+		msgpack_file = file.replace("json", "msgpack")
+		with open(msgpack_file, "wb+") as out_f:
+			packed = msgpack.packb(data)
+			out_f.write(packed)
+
+		# Load and verify the data.
+		with open(msgpack_file, "rb") as in_f:
+			byte_data = in_f.read()
+
+		# Unpack/deserialize the byte data read from the messagepack file.
+		loaded_data = msgpack.unpackb(byte_data)
+
+		# Convert the (loaded) dict object to string. Be sure to sort keys 
+		# to maintain consistency.
+		loaded_data_str = json.dumps(loaded_data, sort_keys=True)
+
+		# Hash the (loaded) data.
+		loaded_data_hash = hashSum(loaded_data_str)
+
+		# Confirm the data matches.
+		assert data == loaded_data
+		assert data_hash == loaded_data_hash
+
+		# Update tracking file.
+		current_progress.append(file)
+		with open("tracking.txt", "w+") as f:
+			f.write("\n".join(current_progress))
+
+	return
+
+
+def main():
+	# Initialize argument parser.
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"--convert",
+		action="store_true",
+		help="Specify whether to restart the preprocessing from scratch. Default is false/not specified."
+	)
+	args = parser.parse_args()
+
+	# Determine which action to do based on the arguments passed in:
+	# either convert all existing metadata from the current format to
+	# the new one (str: List[str] -> str: int) or run a test on a 
+	# select file.
+	if args.convert:
+		convert()
+	else:
+		test()
 
 	# Exit the program.
 	exit(0)
