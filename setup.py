5d0945bf86ca47c3d2721760c91ca11a7cb0d2e1
Diego Magdaleno <dmmagdal@ucsc.edu>
2024-07-03 13:18:33 -0400

    Added setup.py for cythonizing specific functions in search.pyx

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup
+# from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+# extensions = [
+# 	Extension("load_msgpack", ["preprocess_helper.pyx"], libraries=["msgpackc"]),
+# ]
+
+setup(
+	# name="msgpack_loader",
+	# ext_modules=cythonize(extensions),
+	ext_modules=cythonize(
+		"search_helper.pyx", annotate=True
+	),
+)
