9b89d5380b4e33ef42d6c0c75a4315f4261709e9
Diego Magdaleno <dmmagdal@ucsc.edu>
2024-07-11 12:02:31 -0400

    Added some notes to unique_words.py

--- unique_words.py
+++ unique_words.py
@@ -64,7 +64,7 @@ def main():
 		# ] # optimization implemented but VERY SLOW
 		file_unique_words = list(
 			set(word_to_docs.keys()).difference(set(unique_words.keys()))
-		) # optimization implemented and actually resulted in the incremental seedups/optimziation I was looking for
+		) # optimization implemented and actually resulted not freezing (unlike the implementations above)
 		# file_unique_words = list(word_to_docs.keys()) # Unoptimized (not tested for performance)
 
 		# Compute the IDFs for each file unique word.
